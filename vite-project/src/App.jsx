b93be4d5fc6364bde362ecd0bbc02a8950e40db9
kesavindia <kesavindia83@gmail.com>
2023-09-27 15:07:43 +0530

    pages created

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,13 +2,16 @@ import React,{ useState } from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import { AuthContextProvider } from './context/AuthContext'
 export default function App() {
   return (
     <div>
+      <AuthContextProvider>
       <Navbar/>
       <Routes>
         <Route path='/' element ={<Home/>}/>
       </Routes>
+      </AuthContextProvider>
     </div>
   )
 }
